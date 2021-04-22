76cc3a14ef517ad1fd65b28583f98d5b115271b0
paranoidfreek111 <paranoidfreek000@gmail.com>
2021-04-22 22:12:53 +0800

    Saying Hello, world!
    Introducing you and what your intentions are

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, world! My name is Jillian!
+        </p>
+        <p>
+          hey gurl! whats up? chat me if you need some more inspiration!
         </p>
         <a
           className="App-link"
